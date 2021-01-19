9cf3f7b6a68c3d354b1d690528726b092c0714cc
cep104 <candicelizabethpeters@gmail.com>
2021-01-18 21:49:01 -0600

    commit tree 437499727c230cc96ed21de6fed7e1bbdc2e5f0d

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -1,9 +1,41 @@
-import React, { Component } from 'react';
-import 'isomorphic-fetch';
-import MovieReviews from './MovieReviews'
+//last test for LatestMovieReviews container won't pass till we get MovieReviews passing.
+//comment SearchableMovieReviews out if getting errors till you set it up.
 
-const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
-            + `api-key=${NYT_API_KEY}`;
+import React, { Component } from "react";
+import "isomorphic-fetch";
+import MovieReviews from "./MovieReviews";
+
+const NYT_API_KEY = "dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ";
+const URL =
+  "https://api.nytimes.com/svc/movies/v2/reviews/all.json?" +
+  `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends Component {
+  state = {
+    reviews: [],
+  };
+  //make fetch request to get review data and set reviews, data is set under results
+  componentDidMount() {
+    fetch(URL)
+      .then((response) => response.json())
+      .then(
+        (movieData) =>
+          this.setState({
+            reviews: movieData.results,
+          })
+        //   ,() => console.log(this.state)
+        // )
+      );
+  }
+  render() {
+    return (
+      <div className="latest-movie-reviews">
+        <MovieReviews reviews={this.state.reviews} />
+
+        {/* //pass reviews down to movieReviews */}
+      </div>
+    );
+  }
+}
+export default LatestMovieReviewsContainer;
